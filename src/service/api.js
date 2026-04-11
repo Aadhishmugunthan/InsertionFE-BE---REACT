bb8ceb487a1e87eccf23b8a17fcf14fc09f7f7b5
Aadhishmugunthan <aadhishmugunthan2002@gmail.com>
2026-04-11 16:54:47 +0530

    for vercel

--- src/service/api.js
+++ src/service/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/tasks";
+const API_URL = "https://insertionfe-be-springboot.onrender.com/api/tasks";
 
 export const getTasks = () => {
 
